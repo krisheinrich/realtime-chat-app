b3ba2724d5dddd250bc8ecb5aea68765261e1d02
Kris Heinrich <krisheinrich@gmail.com>
2017-05-21 16:04:22 -0700

    setup webpack dev server with Babel and hot reloading

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [
+        'Hey, hows it goin?',
+        'Good :) hbu?'
+      ]
+    };
+  }
+
+  render() {
+    const messageNodes = this.state.messages.map((message, index) => (
+      <div key={index} style={{color: 'purple'}}>{message}</div>
+    ));
+
+    return (
+      <div>{messageNodes}</div>
+    );
+  }
+}
+
+export default App;
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import React, { render } from 'react';
+import App from './components/App.jsx';
+
+render(<App />, document.getElementById('react-root'));
