9e16fdbce11a198b83834aa9ad5e40ffbd996fa4
Kris Heinrich <krisheinrich@gmail.com>
2017-05-22 19:13:46 -0700

    move styles to component files

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import React, { render } from 'react';
 import App from './components/App.jsx';
-require('./styles/main.scss');
+import './styles/main.scss';
 
 render(<App />, document.getElementById('react-root'));
